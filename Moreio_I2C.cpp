3dca98107eac74ce84bf90dcc01f68e26864f930
ulyssedu45 <ulysseballesteros@gmail.com>
2017-09-30 12:05:08 +0200

    Add files via upload

---
+++ Moreio_I2C.cpp
@@ -0,0 +1,119 @@
+#include "Moreio_I2C.h"
+#include <Arduino.h>
+#include <Wire.h>
+
+Moreio_I2C::Moreio_I2C(uint8_t mio_addr)
+{
+  _addr = mio_addr;
+}
+
+void Moreio_I2C::begin() {
+  Wire.begin();
+}
+
+void Moreio_I2C::digitalWrite(int pin, bool value){
+  String data = "00";
+  data = data + String(pin, HEX) + value;
+  Wire.beginTransmission(_addr);
+  for (int i=0; i <= data.length(); i++){
+    Wire.write(data[i]);
+  }
+  Wire.endTransmission();
+}
+
+void Moreio_I2C::analogWrite(int pin, int value){
+  if(value > 255){
+    value = 255;
+  }else if(value < 0){
+    value = 0;
+  }
+  String data = "01";
+  data = data + String(pin, HEX);
+  if(value < 10){
+    data = data + "00" + value;
+  }else if(value >= 10 && value < 100){
+    data = data + "0" + value;
+  }else if(value >= 100){
+    data = data + value;
+  }
+  Wire.beginTransmission(_addr);
+  for (int i=0; i <= data.length(); i++){
+    Wire.write(data[i]);
+  }
+  Wire.endTransmission();
+}
+
+void Moreio_I2C::pinMode(int pin, int value){
+  if(pin > 11){
+    pin = 11;
+  }else if(pin < 0){
+    pin = 0;
+  }
+  String data = "1";
+  data = data + String(pin, HEX) + value;
+  Wire.beginTransmission(_addr);
+  for (int i=0; i <= data.length(); i++){
+    Wire.write(data[i]);
+  }
+  Wire.endTransmission();
+}
+
+int Moreio_I2C::analogRead(int pin){
+  Wire.requestFrom(_addr, 29);
+
+  String data = "";
+  while (Wire.available()) {
+    char c = Wire.read();
+    data = data + c;
+  }
+
+  if(pin == 0){
+    return data.substring(0,4).toInt();
+  }else if(pin == 1){
+    return data.substring(4,8).toInt();
+  }else if(pin == 2){
+    return data.substring(8,12).toInt();
+  }else if(pin == 3){
+    return data.substring(12,16).toInt();
+  }else{
+    return 0;
+  }
+}
+
+int Moreio_I2C::digitalRead(int pin){
+  Wire.requestFrom(_addr, 29);
+
+  String data = "";
+  while (Wire.available()) {
+    char c = Wire.read();
+    data = data + c;
+  }
+  if(pin == 0){
+    return data.substring(16,17).toInt();
+  }else if(pin == 1){
+    return data.substring(17,18).toInt();
+  }else if(pin == 2){
+    return data.substring(18,19).toInt();
+  }else if(pin == 3){
+    return data.substring(19,20).toInt();
+  }else if(pin == 4){
+    return data.substring(20,21).toInt();
+  }else if(pin == 5){
+    return data.substring(21,22).toInt();
+  }else if(pin == 6){
+    return data.substring(22,23).toInt();
+  }else if(pin == 7){
+    return data.substring(23,24).toInt();
+  }else if(pin == 8){
+    return data.substring(24,25).toInt();
+  }else if(pin == 9){
+    return data.substring(25,26).toInt();
+  }else if(pin == 10){
+    return data.substring(26,27).toInt();
+  }else if(pin == 11){
+    return data.substring(27,28).toInt();
+  }else{
+    return 0;
+  }
+}
+
---
+++ Moreio_I2C.h
@@ -0,0 +1,27 @@
+#ifndef FDB_MOREIO_I2C_H
+#define FDB_MOREIO_I2C_H
+
+#include <Arduino.h>
+
+class Moreio_I2C {
+public:
+
+  Moreio_I2C(uint8_t lcd_addr);
+
+  void begin();
+
+  void digitalWrite(int pin, bool value);
+
+  void analogWrite(int pin, int value);
+
+  void pinMode(int pin, int value);
+
+  int analogRead(int pin);
+
+  int digitalRead(int pin);
+  
+private:
+  uint8_t _addr;
+};
+
+#endif // FDB_MOREIO_I2C_H
